06e668e6acae8c659842698ddf7316410434a7cb
dngwoodo <dngwoodo@gmail.com>
2021-07-28 00:52:44 +0900

    feat: Implement todo app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>Todo-App</h1>
+    <div class="todo-control">
+      <input type="text" placeholder="할 일을 입력해주세요" v-model="todo" />
+      <button type="button" @click="handleClickAddTodo" data-testid="add-todo">
+        추가
+      </button>
+    </div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <p @click="handleClickToggle(todo)" :class="todo.done ? 'done' : ''">
+          {{ todo.text }}
+        </p>
+        <button
+          type="button"
+          @click="handleClickDeleteTodo(todo.id)"
+          data-testid="delete-todo"
+        >
+          삭제
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld/index.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      todo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    handleClickAddTodo() {
+      if (!this.todo.trim()) {
+        return;
+      }
+
+      this.todos.push({
+        id: +new Date(),
+        text: this.todo,
+        done: false,
+      });
+      this.todo = "";
+    },
+    handleClickDeleteTodo(todoId) {
+      this.todos = this.todos.filter(({ id }) => id !== todoId);
+    },
+    handleClickToggle(todo) {
+      todo.done = !todo.done;
+    },
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.done {
+  text-decoration: line-through;
+  color: grey;
 }
 </style>
